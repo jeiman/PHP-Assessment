5732a25e929d35108a58ea9ff4a77ad39fdfc8ca
Jeiman <johnfaith7@gmail.com>
2016-04-08 00:29:37 +0100

    View page for CSV File

--- App/Views/viewCSV.php
+++ App/Views/viewCSV.php
@@ -1,7 +1,13 @@
 <?php
 require_once 'App/Models/csvImport.php';
 
-$csv = new csvImport( '../assets/example.csv' );
-$data = $csv->getData();
+$csv = new csvImport( 'assets/example.csv' );
+$data = $csv->getDataFromCSV();
 print_r( $data );
+
+echo '<pre>'.json_encode($data) . '</pre>';
+
+// $fp = fopen('assets/results.json', 'w');
+// fwrite($fp, json_encode($data));
+// fclose($fp);
 ?>
