3e8dc1a25a5e8664565c50969fd70131eb023018
Jeiman <johnfaith7@gmail.com>
2016-04-08 00:31:07 +0100

    Main page to load entire project

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
-// echo '<pre>';
-// require_once 'App/Views/viewCSV.php';
+
+// public function __autoload($class) {
+//   require_once 'App/'.$class.'.php';
+// }
 
 
-$myfile = fopen("example.csv", "a+") or die("Unable to open file!");
-$txt = "John Doe,39403904,San Jose 21\n";
+$myfile = fopen("assets/example.csv", "a+") or die("Unable to open file!");
+$txt = "Singam Jeya,49328498234892,London UK\n";
 fwrite($myfile, $txt);
-$txt = "Jane Doe,4938928,San Fran 23\n";
-fwrite($myfile, $txt);
+//$txt = "Jane Doe,4938928,San Fran 23\n";
+//fwrite($myfile, $txt);
 fclose($myfile);
+
+echo '<pre>';
+require_once 'App/Views/viewCSV.php';
 
 // $file="assets/example.csv";
 // $csv= file_get_contents($file);
