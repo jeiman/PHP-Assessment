5f17c3c53e18c9d671a318a6374c5c51ce1d96c1
Jeiman <johnfaith7@gmail.com>
2016-04-08 00:28:31 +0100

    Controller for CRUD operations
    Added functions for CRUD operations

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -8,5 +8,18 @@ class AppController {
 
   }
 
-  public function
+  public function update() {
+
+    if isset($_GET['update']){
+
+      $updateValue = $_GET['update'];
+
+    }
+
+  }
+
+  public function delete() {
+
+  }
+
 }
